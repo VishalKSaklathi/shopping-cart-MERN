6e9e0ee9088c8e1c0514ccee1fbe9fab26550d59
VishalKSaklathi <vishal.krishnamurthi.saklathi@gmail.com>
2025-07-17 21:13:32 +0530

    rduced

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const db = require('./db'); // assuming db.js
 const authRoutes = require('./routes/authRoutes'); // assuming authRoutes.js
 const razorPayments = require('./routes/razorPayments'); // assuming razorPayments.js
-
+const cartRoutes = require('./routes/cartRoutes'); // assuming cartRoutes.js
 // import bodyParser from 'body-parser';
 // const { json } = bodyParser;
 
@@ -23,75 +23,15 @@ app.use(express.json());
 //login / signup hndling
 app.use('/api/auth', authRoutes);
 app.use('/api/payment', razorPayments);
-// add to cart
-app.post('/api/cart', (req, res) => {
-    const { productID, userID, title, price, quantity, thumbnail } = req.body;
-    console.log(" Incoming cart item:", req.body);
+app.use('/api/cart', cartRoutes);
 
-    const sql = 'INSERT INTO cart_table (productID, userID, title, price, quantity, thumbnail) VALUES (?, ?, ?, ?, ?, ?)';
-    db.query(sql, [productID, userID, title, price, quantity, thumbnail], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json({ message: 'Item added to cart!', result });
-    });
-});
-//get cart items for a specific user
-app.get('/api/cart/:userID', (req, res) => {
-    const { userID } = req.params;
-    const sql = 'SELECT * FROM cart_table WHERE userID = ?';
-    db.query(sql, [userID], (err, result) => {
-        if (err) {
-            console.error('Error fetching cart items:', err);
-            return res.status(500).send(err);
-        }
-        res.send(result);
-    });
-});
-
-
-// Update quantity
-app.put('/api/cart/:productID', (req, res) => {
-    const { productID } = req.params;
-    const { quantity, userID } = req.body;
-
-
-    const sql = 'UPDATE cart_table SET quantity = ? WHERE  userID =? AND productID = ? ';
-    db.query(sql, [quantity, userID, productID], (err, result) => {
-        if (err) {
-            console.error(" SQL Update Error:", err);
-            return res.status(500).send(err);
-        }
-        res.send({ message: 'Quantity updated!', result });
-    });
-});
-
-// Remove item from cart
-app.delete('/api/cart/:productID', (req, res) => {
-    const { productID } = req.params;
-
-    const sql = 'DELETE FROM cart_table WHERE productID = ?';
-    db.query(sql, [productID], (err, result) => {
-        if (err) {
-            console.error(" SQL Delete Error:", err);
-            return res.status(500).send(err);
-        }
-        res.send({ message: 'Item removed from cart!', result });
-    });
-});
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello from Vercel backend!');
 });
-//i have to create another endpoint to delete all Items related to userID
-
-const PORT = process.env.PORT || 5000;
-
-module.exports = (req, res) => {
-    res.status(200).send("API working");
-};
 
 // process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on http://127.0.0.1:${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,62 @@
+const db = require('../db');
+const express = require('express');
+const router = express.Router();
+
+// add to cart
+router.post('/', (req, res) => {
+    const { productID, userID, title, price, quantity, thumbnail } = req.body;
+    console.log(" Incoming cart item:", req.body);
+
+    const sql = 'INSERT INTO cart_table (productID, userID, title, price, quantity, thumbnail) VALUES (?, ?, ?, ?, ?, ?)';
+    db.query(sql, [productID, userID, title, price, quantity, thumbnail], (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json({ message: 'Item added to cart!', result });
+    });
+});
+//get cart items for a specific user
+router.get('/:userID', (req, res) => {
+    const { userID } = req.params;
+    const sql = 'SELECT * FROM cart_table WHERE userID = ?';
+    db.query(sql, [userID], (err, result) => {
+        if (err) {
+            console.error('Error fetching cart items:', err);
+            return res.status(500).send(err);
+        }
+        res.send(result);
+    });
+});
+
+
+// Update quantity
+router.put('/:productID', (req, res) => {
+    const { productID } = req.params;
+    const { quantity, userID } = req.body;
+
+
+    const sql = 'UPDATE cart_table SET quantity = ? WHERE  userID =? AND productID = ? ';
+    db.query(sql, [quantity, userID, productID], (err, result) => {
+        if (err) {
+            console.error(" SQL Update Error:", err);
+            return res.status(500).send(err);
+        }
+        res.send({ message: 'Quantity updated!', result });
+    });
+});
+
+// Remove item from cart
+router.delete('/:productID', (req, res) => {
+    const { productID } = req.params;
+
+    const sql = 'DELETE FROM cart_table WHERE productID = ?';
+    db.query(sql, [productID], (err, result) => {
+        if (err) {
+            console.error(" SQL Delete Error:", err);
+            return res.status(500).send(err);
+        }
+        res.send({ message: 'Item removed from cart!', result });
+    });
+});
+
+module.exports = router;
