3982ad9c3c964cac1d0715750616a639c655a920
VishalKSaklathi <vishal.krishnamurthi.saklathi@gmail.com>
2025-07-19 01:44:29 +0530

    updated DB connection

--- backend/db.js
+++ backend/db.js
@@ -3,22 +3,25 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const util = require('util');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     ssl: {
         rejectUnauthorized: true
     }
 });
 db.query = util.promisify(db.query);
-db.connect(err => {
-    if (err) {
-        console.error(' DB connection error:', err.message);
-        return;
-    }
-    console.log('Connected to MySQL DB');
-});
+// db.connect(err => {
+//     if (err) {
+//         console.error(' DB connection error:', err.message);
+//         return;
+//     }
+//     console.log('Connected to MySQL DB');
+// });
 
 module.exports = db;
