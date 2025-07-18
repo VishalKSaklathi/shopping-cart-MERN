95350e1f469e4b3d9258065e024f8e54cbb43a9c
VishalKSaklathi <vishal.krishnamurthi.saklathi@gmail.com>
2025-07-18 11:28:06 +0530

    CORS update

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,7 @@ const app = express();
 const allowedOrigin = process.env.CLIENT_URL;
 
 app.use(cors({
-    origin: function (origin, callback) {
-        console.log("Incoming Origin:", origin);
-        if (!origin || origin === allowedOrigin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: allowedOrigin,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
