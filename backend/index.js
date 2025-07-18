4e8fc0635c8b30387f78057360b2ab12d8cb1935
VishalKSaklathi <vishal.krishnamurthi.saklathi@gmail.com>
2025-07-18 11:47:57 +0530

    *

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser())
